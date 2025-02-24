0f84d030cc891026f13bdc8d055917ea30f1e796
RadyaI <radyaiftikhar@gmail.com>
2025-02-24 19:43:06 +0700

    Ignore eslint

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -4,10 +4,18 @@ export const metadata: Metadata = {
     title: "View - url short"
 }
 
+async function get() {
+    try {
+        
+    } catch (error: any) {
+        console.log(error.message)
+    }
+}
+
 export default function ViewLink() {
-    return(
+    return (
         <>
-            View Link
+
         </>
     )
 }
