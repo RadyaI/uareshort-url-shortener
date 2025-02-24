3826e855a49eaab2c26493bb9a8b5d65aead07ee
RadyaI <radyaiftikhar@gmail.com>
2025-02-24 21:41:52 +0700

    fix tracking

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -25,6 +25,7 @@ export default function Redirect() {
         try {
             await addDoc(collection(db, "tracking"), {
                 ip: await getIp(),
+                shortCode: linkId,
                 clickedAt: Timestamp.now().toMillis()
             })
 
