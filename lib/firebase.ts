ea34b8e58ef7c2fba034a93380642dcd2aff2f69
RadyaI <radyaiftikhar@gmail.com>
2025-02-24 21:38:20 +0700

    Update firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    authDomain: "url-shortener-d4c1a.firebaseapp.com",
+    projectId: "url-shortener-d4c1a",
+    storageBucket: "url-shortener-d4c1a.firebasestorage.app",
+    messagingSenderId: "630312324218",
+    appId: "1:630312324218:web:db2b8b8226fa15dc027fc3",
+    measurementId: "G-9601VZ45LM"
 };
 
 
